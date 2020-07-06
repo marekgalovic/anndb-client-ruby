d2b5d15129b6777593b07e150fb00f8fd275c818
Marek Galovic <galovic.galovic@gmail.com>
2020-07-06 13:38:27 +0200

    Configure

--- lib/anndb.rb
+++ lib/anndb.rb
@@ -1,2 +1,26 @@
 require 'anndb/client'
 require 'dataset_pb'
+
+module Anndb
+  class << self
+    attr_accessor :configuration
+    attr_accessor :client_instance
+  end
+
+  def self.configure
+    self.configuration ||= Config.new
+    yield configuration
+  end
+
+  def self.instance
+    self.client_instance ||= Anndb::Client.new(self.configuration.nodes.sample)
+  end
+
+  class Config
+    attr_accessor :nodes
+
+    def initialize
+      @nodes = []
+    end
+  end
+end
